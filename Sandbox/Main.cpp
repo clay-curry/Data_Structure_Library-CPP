15b1c920a6a94a650a0516bfe365b9b907d6b1f1
Clayton Curry <claycurry34@ou.edu>
2020-09-11 22:36:02 -0500

    created a sandbox project for miscellaneous testing

--- Sandbox/Main.cpp
+++ Sandbox/Main.cpp
@@ -1,43 +1,83 @@
 #include<iostream>
-template<typename DT>
-class Object
+#include<vector>
+#include<stack>
+#include<math.h>
+using namespace std;
+
+vector<int> parseIntArray(string s)
 {
-private:
-	static int count;
-	DT* info
-public:
-	Object(const DT& val)
-	{
+    s = s.substr(1, s.size() - 2);      // trims off '[' ']'
+    char* reverse = new char[s.size()]; // reverses user's input to parse each element by place value order
+    size_t i = s.size();                // iterator starts at the end of the string
+    int element = 0, digit = 0, placeValue = 0;
+    stack<int> inputs;                // inputs are stacked in reverse order, and popped back in order
+    while (i != 0)
+    {
+        while (i != 0 && (digit = ((int)s[--i])) > 47 && digit < 58)
+        {
+            element += (digit - 48) * (pow(10, placeValue));
+            placeValue++;
+        }
+        inputs.push(element);
+        placeValue = digit = element = 0;
+    }
+    vector<int> response(inputs.size());
+    while (!inputs.empty())
+    {
+        response[element++] = inputs.top();
+        inputs.pop();
+    }
 
-	}
-
-};
-
-int value()
+    return response;
+}
+ostream& operator<<(ostream& s, vector<int> v)
 {
-	return 10;
+    s << "{";
+    if (v.size() > 0) s << v[0];
+    for (int i = 1; i < v.size(); i++)
+        s << ", " << v[i];
+    s << "}";
+    return s;
 }
 
-int& svalue()
+int findNumbers(vector<int>&& nums)
 {
-	static int value = 10;
-	return value;
+    int digCt = 0, count = 0;
+    for (int i : nums)
+    {
+        while (i = (i / 10) > 0)
+        {
+            digCt++;
+        }
+        if (digCt % 2 == 0) count++;
+        digCt = 1;
+    };
+    return count;
 }
 
-void setValue(int i)
+int sumEvenDigits(const vector<int>& nums)
 {
+    int count = 0;
+    int logb10;
+    float logi, log10 = log(10);
+    for (int i : nums) {
+        logi = log(i);
+        logb10 = logi / log10;
+        if(logb10 % 2 == 1)
+            count++;
+    }
 
+    return count;
 }
 
+// Find pair with given sum in the array
 int main()
 {
-	// To demonstrate the differences between lvalues, rvalues, lvalue references, and rvalue references
-	int i = 10; // on the left is a variable 'i' cooresponding to a (l)ocation in memory. 'i' is an lvalue
-				// on the right is a value which C++ gives no permenant place in memory. 10 is an rvalue
-	
-	int a = value();	//  on the right is a temporary value which is created by the function. the expression on the right is an rvalue
-	svalue() = a;		// the expression on the left returns a permenant value in memory, meaning it is an lvalue
-	
-	setValue(a);	// valid since the signiture (int i) can accept an lvalue or rvalue
-	setValue(svalue());
+    string input;
+    cin >> input;
+    vector<int> parseArray = parseIntArray(input);
+    cout << sumEvenDigits(parseArray);
+
+
+
 }
